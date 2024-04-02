685dcb5b5c27c1fdf9a0ecf8f002135ce5b3b283
bentran1vn <tan182205@gmail.com>
2024-04-02 15:56:12 +0700

    Config environment

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+    // Cấu hình đường dẫn src cho Terminal hiểu, tránh lỗi import
+  }
+})

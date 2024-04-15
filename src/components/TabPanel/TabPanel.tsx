a22396e12117da3372b014381decc69c9b34e5d9
bentran1vn <tan182205@gmail.com>
2024-04-15 17:34:41 +0700

    Create Dev Branch

--- src/components/TabPanel/TabPanel.tsx
+++ src/components/TabPanel/TabPanel.tsx
@@ -43,7 +43,7 @@ export function a11yProps(index: number) {
 export default function VerticalTabs() {
   const [value, setValue] = React.useState(1)
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
   }
   return (
